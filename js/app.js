7854c21be054dbed4829da0c8141185d05837982
Vincent <vincenttchang90@gmail.com>
2017-10-19 12:10:59 -0400

    added error handling and css

--- js/app.js
+++ js/app.js
@@ -1,66 +1,3 @@
-var casinos = [
-    {
-        name: "ARIA Resort & Casino Las Vegas",
-        location: {lat: 36.107424, lng: -115.176684},
-    },
-    {
-        name: "Bally's Las Vegas Hotel & Casino",
-        location: {lat: 36.114078, lng: -115.170638},
-    },
-    {
-        name: "Bellagio Hotel and Casino",
-        location: {lat: 36.112626, lng: -115.176705},
-    },
-    {
-        name: "Caesars Palace",
-        location: {lat: 36.116160, lng: -115.174531},
-    },
-    {
-        name: "Excalibur Hotel & Casino",
-        location: {lat: 36.098982, lng: -115.175462},
-    },
-    {
-        name: "Flamingo Las Vegas",
-        location: {lat: 36.116458, lng: -115.170738},
-    },
-    {
-        name: "Harrah's Las Vegas Hotel & Casino",
-        location: {lat: 36.119464, lng: -115.171618},
-    },
-    {
-        name: "Mandalay Bay",
-        location: {lat: 36.091852, lng: -115.175213},
-    },
-    {
-        name: "MGM Grand",
-        location: {lat: 36.102550, lng: -115.170253},
-    },
-    {
-        name: "The Mirage",
-        location: {lat: 36.121213, lng: -115.174084},
-    },
-    {
-        name: "Planet Hollywood Resort & Casino",
-        location: {lat: 36.109996, lng: -115.170097},
-    },
-    {
-        name: "Rio All-Suites Hotel and Casino",
-        location: {lat: 36.117515, lng: -115.188159},
-    },
-    {
-        name: "Treasure Island",
-        location: {lat: 36.124727, lng: -115.171866},
-    },
-    {
-        name: "The Venetian",
-        location: {lat: 36.121174, lng: -115.169653},
-    },
-    {
-        name: "Wynn Las Vegas",
-        location: {lat: 36.126498, lng: -115.165728},
-    }
-];
-
 let map;
 let currentInfoWindow;
 
@@ -91,9 +28,17 @@ var Casino = function(data) {
             self.url = "";
         }
         self.street = results.location.formattedAddress[0];
+        if (typeof self.street === 'undefined'){
+            self.street = "";
+        }
         self.city = results.location.formattedAddress[1];
+        if (typeof self.city === 'undefined'){
+            self.city = "";
+        }
         self.phone = results.contact.formattedPhone;
-
+        if (typeof self.phone === 'undefined'){
+            self.phone = "";
+        }
         self.infoWindow.setContent(
         '<div><b>' + self.name + "</b></div>" +
         '<div>' + self.street + "</div>" +
